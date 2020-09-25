9ca4a6a9552225dd046c8d61a9a7873ac7eb0c60
İbrahim Uzun <info@ibrahimuzun.com>
2020-09-26 02:47:25 +0300

    footer

--- components/sections/footer.tsx
+++ components/sections/footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Footer: React.FC = () => {
   return (
-    <div className="w-full fixed bottom-0 text-center text-sm p-4 mt-8">
+    <div className="w-full bottom-0 text-center text-sm p-4 mt-8">
       © {new Date().getFullYear()} · İbrahim Uzun {" "}
     </div>
   );
