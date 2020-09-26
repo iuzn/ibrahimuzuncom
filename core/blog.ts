9243055d8ca9f3b25d3dc0fef5c5b0d711345ade
İbrahim Uzun <info@ibrahimuzun.com>
2020-09-27 00:46:20 +0300

    elementlab.net use for api

--- core/blog.ts
+++ core/blog.ts
@@ -2,13 +2,13 @@ import fetch from "node-fetch";
 import { BlockMapType } from "react-notion";
 
 export const getBlogTable = async <T>(blogId: string): Promise<T[]> =>
-  fetch(`https://notion-api.splitbee.io/v1/table/${blogId}`).then(res =>
+  fetch(`https://notion-api.elementlab.net/v1/table/${blogId}`).then(res =>
     res.json()
   );
 
 export const getPageBlocks = async (pageId: string): Promise<BlockMapType> => {
   return await fetch(
-    `https://notion-api.splitbee.io/v1/page/${pageId}`
+    `https://notion-api.elementlab.net/v1/page/${pageId}`
   ).then(res => res.json());
 };
 
